ebf595c5df280ad6fe41c84b72f22cca08877e25
Jean-kassyl <kassylmac@gmail.com>
2022-09-24 00:31:14 +0100

    feat: add the about section - add some text - style not satisfying, have to keep working on that

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -15,19 +15,41 @@ const Home = () => {
 
                 <section className="about">
                     <div className="text">
-                        <p>
+                        <p className="paragraph1">
                             Hi I am jean kassyl,
                             A highly motivated fullstack developer who is passionate about using code to build 
                             necessary and helpful software.
                         </p>
-                        <p>
+                        <p className="marg">
                             I have always been concerned about giving back to others through my work and that’s why I volunteered a lot when studying social science. 
                             I participated on project which aims to empower peoples to fight against hunger, illiteracy and deprivation. 
                             With adding values to people’s life as an end goal, I’ve always put my all self into every project I participated in.
                         </p>
-                        <p>
+                        <p className="marg">
                             today, I am looking for a role that will let me keep working towards making people’s day to day life easier
                         </p>
+                        <p className="marg">
+                            Some of the tools I work with:
+                        </p>
+                        <div className="skills">
+                            <ul>
+                                <li>Python</li>
+                                <li>Javascript</li>
+                                <li>Postgres</li>
+                                <li>ORM</li>
+                                <li>Git</li>
+                                <li>Github</li>
+                            </ul>
+                            <ul>
+                                <li>Flask</li>
+                                <li>Node</li>
+                                <li>React</li>
+                                <li>VueJS</li>
+                                <li>Tailwind</li>
+                                <li>Sass</li>
+                                <li>GraphQL</li>
+                            </ul>
+                        </div>     
                     </div>
                     <div>
                         <h2 className="about-title"><span>About</span><span>.</span><span>/</span></h2>
