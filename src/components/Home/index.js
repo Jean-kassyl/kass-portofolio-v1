43b23a9cb677928ba430f4d3ba5dad5952aa6eeb
Jean-kassyl <kassylmac@gmail.com>
2022-09-25 20:06:35 +0100

    feat: add the mobile version of the main section

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,30 +9,32 @@ const Home = () => {
         <div>
             <Header />
             <main>
-                <div>
+                <div className="padd">
                     <h1>Hi, I am Kassyl, A Software Developer</h1>
                     <p> who is passionate about using code to build easy and<br /> helpful web solutions <span>!</span></p>
                 </div>
 
                 <section className="about">
                     <div className="text">
-                        <p className="paragraph1">
+                        <p className="paragraph1 padd">
                             Hi I am jean kassyl,
                             A highly motivated fullstack developer who is passionate about using code to build 
                             necessary and helpful software.
                         </p>
-                        <p className="marg">
-                            I have always been concerned about giving back to others through my work and that’s why I volunteered a lot when studying social science. 
-                            I participated on project which aims to empower peoples to fight against hunger, illiteracy and deprivation. 
-                            With adding values to people’s life as an end goal, I’ve always put my all self into every project I participated in.
-                        </p>
-                        <p className="marg">
-                            today, I am looking for a role that will let me keep working towards making people’s day to day life easier
-                        </p>
-                        <p className="marg">
+                        <div className="mobile-back padd">
+                            <p className="marg">
+                                I have always been concerned about giving back to others through my work and that’s why I volunteered a lot when studying social science. 
+                                I participated on project which aims to empower peoples to fight against hunger, illiteracy and deprivation. 
+                                With adding values to people’s life as an end goal, I’ve always put my all self into every project I participated in.
+                            </p>
+                            <p className="marg">
+                                today, I am looking for a role that will let me keep working towards making people’s day to day life easier
+                            </p>
+                        </div>
+                        <p className="marg padd">
                             Some of the tools I work with:
                         </p>
-                        <div className="skills">
+                        <div className="skills padd">
                             <ul>
                                 <li>Python</li>
                                 <li>Javascript</li>
@@ -53,7 +55,7 @@ const Home = () => {
                         </div>     
                     </div>
                     <div>
-                        <h2 className="heading2"><span>About</span><span>.</span><span>/</span></h2>
+                        <h2 className="heading2 padd"><span>About</span><span>.</span><span>/</span></h2>
                     </div>
                 </section>
 
