ae5409165b3cd619a14dde4e17be624644957adc
Jean-kassyl <kassylmac@gmail.com>
2022-09-26 14:06:45 +0100

    feat: add the menu toggling logic on the mobile view

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -4,13 +4,22 @@ import {Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import {useRef} from 'react';
 
 
 const Header = () => {
+    
+    const toggleMenu = () => {
+        ulRef.current.classList.toggle('toggling')
+    }
+
+   const ulRef = useRef(null)
+   
     return (
         <header>
             <img src={Logo} alt="logo" className="logo" />
-            <ul className="items mobile">
+    
+            <ul  ref={ulRef} className="items mobile" >
                 <Link to='/'>
                     <li> Home</li>
                 </Link>
@@ -18,6 +27,7 @@ const Header = () => {
                     <li> Projects</li>
                 </Link>
             </ul>
+
             <ul className="items">
                 <Link to='/'>
                     <li><FontAwesomeIcon icon={faGithub} className="socialIcon" /> </li>
@@ -25,7 +35,7 @@ const Header = () => {
                 <Link to='/'>
                     <li><FontAwesomeIcon icon={faLinkedin} className="socialIcon" /> </li>
                 </Link>
-                <FontAwesomeIcon icon={faBars} className="mobileMenu" />
+                <FontAwesomeIcon icon={faBars} className="mobileMenu" onClick={toggleMenu}/>
             </ul>
             
         </header>
