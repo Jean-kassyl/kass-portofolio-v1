e535a3fc71140e7d6915963cadd96a424f829c17
Jean-kassyl <kassylmac@gmail.com>
2022-09-26 22:00:43 +0100

    feat: add the logic for the filter button

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -4,9 +4,21 @@ import Footer from '../common/Footer';
 import Card from '../common/ProjectCard';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import {useRef, useState} from 'react'
 
 
 const Projects = () => {
+
+    const [selectedOption, setSelectedOption] = useState("All")
+    const categories = useRef(null)
+
+    const handleClick = () => {
+        categories.current.classList.toggle('show')
+    }
+
+    const handleChange = (e) => {
+        setSelectedOption(e.target.value)
+    }
     return (
         <>
             <Header />
@@ -18,19 +30,43 @@ const Projects = () => {
                     <div className="input-block">
                         <input type="text" />
                         <FontAwesomeIcon icon={faSearch} className="search"/>
-                        <button>All</button>
-                        <div className="categories">
+                        <button onClick={handleClick}>{selectedOption}</button>
+                        <div className="categories" ref={categories}>
                             <label>
-                                <input type="radio" name="category"/>All
+                                <input 
+                                    type="radio" 
+                                    name="category" 
+                                    value="All" 
+                                    checked={selectedOption === "All"}
+                                    onChange={handleChange}
+                                />All
                             </label>
                             <label>
-                                <input type="radio" name="category"/>Frontend
+                                <input 
+                                    type="radio" 
+                                    name="category" 
+                                    value="Frontend"
+                                    checked={selectedOption === "Frontend"}
+                                    onChange={handleChange}
+                                />Frontend
                             </label>
                             <label>
-                                <input type="radio" name="category"/>Backend
+                                <input 
+                                    type="radio" 
+                                    name="category" 
+                                    value="Backend"
+                                    checked={selectedOption === "Backend"}
+                                    onChange={handleChange}
+                                />Backend
                             </label>
                             <label>
-                                <input type="radio" name="category"/>Fulstack
+                                <input 
+                                    type="radio" 
+                                    name="category" 
+                                    value="Fullstack"
+                                    checked={selectedOption === "Fullstack"}
+                                    onChange={handleChange}
+                                />Fullstack
                             </label>
                         </div>
                     </div>
